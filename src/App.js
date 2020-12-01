be8cf0156e6e9580c58513b5a75e63e08740172d
LAPTOP-CPQLSHDE\littlemag126 <callerofhope7@gmail.com>
2020-11-30 19:51:17 -0600

    First Commit!

--- src/App.js
+++ src/App.js
@@ -12,15 +12,12 @@ import Select from 'react-select';
 import 'medium-editor/dist/css/medium-editor.css';
 import 'medium-editor/dist/css/themes/default.css';
 import './App.css';
-import globals from './globals'
-import { fetchQuestion, fetchRandomQuestion } from './webserviceCalls';
+import { fetchQuestion } from './webserviceCalls';
 import chapters from './constants';
 
-const QUIZZES = globals.QUIZ_GLOBAL
-
 // Websocket server
-// var server = 'http://127.0.0.1:8000/'
-var server = 'wss://mysterious-journey-90036.herokuapp.com'
+var server = 'http://127.0.0.1:8000/'
+// var server = 'wss://mysterious-journey-90036.herokuapp.com'
 const io = require('socket.io-client');
 var user_room = prompt("Please enter your room #", "room");
 var entered_username = prompt("Please enter your user name. E.G. 1-Jeff-Gnarwhals3.0", "Username");
@@ -38,13 +35,10 @@ class App extends Component {
       q_text_to_display: "",
       question_reference: "",
       question_number: 0,
-      question_type: "",
-      is_bonus: false,
       i: 0,
       full_question_text: "",
       answer_question_text: "🤔",
       room: user_room,
-      quizNumber: "1",
       play_audio: false,
       quizzers_in_room: [],
       quiz_started: false,
@@ -52,14 +46,7 @@ class App extends Component {
     };
   }
 
-  // Question iterators
-  bonusQuestionNumber = 0
   showMoreOpions = true;
-
-  // Quiz Questions
-  questionIDs = QUIZZES.quiz1.qs
-  bonusQuestionIDs = QUIZZES.quiz1.bonus
-  selectedRandomQuestionType = 1;
 
   /* When content changes, we send the
 current content of the editor to the server. */
@@ -67,8 +54,6 @@ current content of the editor to the server. */
     var {
       q_text_to_display,
       question_number,
-      question_type,
-      is_bonus,
       full_question_text,
       quiz_started
     } = this.state
@@ -76,8 +61,6 @@ current content of the editor to the server. */
       content: q_text_to_display,
       full_question_text: full_question_text,
       question_number: question_number,
-      question_type: question_type,
-      is_bonus: is_bonus,
       room: room_id,
       quiz_started: quiz_started
     }));
@@ -132,8 +115,6 @@ current content of the editor to the server. */
       stateToChange.q_text_to_display = dataFromServer.question
       stateToChange.full_question_text = dataFromServer.full_question_text
       stateToChange.question_number = dataFromServer.question_number
-      stateToChange.question_type = dataFromServer.question_type
-      stateToChange.is_bonus = dataFromServer.is_bonus
       stateToChange.quiz_started = dataFromServer.quiz_started
 
       // Speaks the text aloud.
@@ -181,7 +162,6 @@ current content of the editor to the server. */
     var {
       q_text_to_display,
       question_number,
-      question_type,
       i,
       full_question_text,
       room,
@@ -190,7 +170,7 @@ current content of the editor to the server. */
     if (i < this.question_array.length) {
       q_text_to_display = q_text_to_display.concat(this.question_array[i]).concat(' ')
       i++
-      this.setState({ q_text_to_display: q_text_to_display, i: i, question_number:question_number, question_type:question_type, full_question_text:full_question_text, quiz_started: true})
+      this.setState({ q_text_to_display: q_text_to_display, i: i, question_number:question_number, full_question_text:full_question_text, quiz_started: true})
       this.sync(room)
     }
   }
@@ -210,53 +190,7 @@ current content of the editor to the server. */
     }
   }
 
-  async nextQuestion(isNextBonus) {
-    this.setState({ jumper: null, is_bonus: isNextBonus })
-    var { question_number } = this.state
-    let questionsList = isNextBonus ? this.bonusQuestionIDs : this.questionIDs
-    let questionNUMTemp = isNextBonus ? this.bonusQuestionNumber : question_number
-    if (questionNUMTemp < questionsList.length) {
-      var questionID = questionsList[questionNUMTemp]
-      console.log('question number:')
-      console.log(questionNUMTemp)
-      await fetchQuestion(questionID)
-        .then(res => res.json()).then((data) => {
-          console.log('question from api!')
-          console.log(data)
-          this.i = 0
-          if (isNextBonus) {
-            this.bonusQuestionNumber++
-            this.setState({
-              question_type: data[0],
-              full_question_text: data[1],
-              q_text_to_display: data[1],
-              answer_question_text: data[2],
-              question_reference: data[3],
-              question_number: question_number,
-              i: data[1].length
-            })                // Add two spaces to ensure no words get read aloud.
-            this.sync(this.state.room)
-          } else {
-            this.setState({
-              question_type: data[0],
-              full_question_text: data[1],
-              answer_question_text: data[2],
-              question_reference: data[3],
-              q_text_to_display: " ",
-              question_number: question_number+1,
-              i: this.i
-            })
-          }
-        });
-    } else {
-      this.setState({
-        q_text_to_display: "*** Out of questions :/ ***",
-        full_question_text: "*** Out of questions :/ ***"
-      })
-    }
-  }
-
-  async randomQuestion() {
+  async nextQuestion() {
     this.setState({ jumper: null })
     var { question_number } = this.state
     var selectedRandomChaptersList = []
@@ -267,16 +201,23 @@ current content of the editor to the server. */
         selectedRandomChaptersList.push(this.state.selectedChapters[i].value);
       }
     }
-    await fetchRandomQuestion(this.selectedRandomQuestionType, 1, selectedRandomChaptersList)
+    await fetchQuestion("Matthew", selectedRandomChaptersList)
       .then(res => res.json()).then((data) => {
         this.i = 0
         if (data != null) {
           console.log(data);
+          console.log("Full quesiton from Database: ", data["Question"]);
+          var fullQuestionTemp
+          if(data["Question"].includes("[")){
+            const removedORquestion = data["Question"].split(" [")
+            fullQuestionTemp = data["Book"]+" "+data["Chapter"]+" verse "+data["Verse"]+" -"+removedORquestion[0]+"?";
+          }else{
+            fullQuestionTemp = data["Book"]+" "+data["Chapter"]+" verse "+data["Verse"]+" -"+data["Question"];
+          }
           this.setState({
-            full_question_text: data[15],
-            question_reference: data[14],
-            question_type: data[18] + " " + data[9],
-            answer_question_text: data[11],
+            full_question_text: fullQuestionTemp,
+            question_reference: data["Reference Text"],
+            answer_question_text: data["Answer"],
             q_text_to_display: " ",
             question_number: question_number+1,
             i: this.i
@@ -293,73 +234,32 @@ current content of the editor to the server. */
       });
   }
 
-  setQuizNumber(selectedQuizNumber) {
-    this.setState({ 
-      quizNumber: selectedQuizNumber,
-      question_number: 0
-    })
-
-    let selected_quiz = QUIZZES[`quiz${selectedQuizNumber}`]
-    this.questionIDs = selected_quiz.qs
-    this.bonusQuestionIDs = selected_quiz.bonus
-  }
-
   showMoreQuizControls = () => {
-    if (this.state.quizNumber === 'practice') {
-      return (
-        <div>
-          <label htmlFor="questionTypeLabel">Choose a question type:</label>
-          <select onChange={(e) => this.selectedRandomQuestionType = e.target.value} name="questionType" id="questionType">
-            <option value="1">General</option>
-            <option value="2">Two Part</option>
-            <option value="3">Three Part</option>
-            <option value="4">Four Part</option>
-            <option value="5">Five Part</option>
-            <option value="6">Multiple Part</option>
-            <option value="7">FTV</option>
-            <option value="8">Reference</option>
-            <option value="9">Situation</option>
-          </select>
-          <div>
-            <label htmlFor="questionChaptersLabel">Choose Chapters:</label>
-            <Select
-              defaultValue={[chapters[0], chapters[1], chapters[2], chapters[3], chapters[4], chapters[5], chapters[6], chapters[7], chapters[8], chapters[9], chapters[10], chapters[11], chapters[12], chapters[13], chapters[14], chapters[15], chapters[16], chapters[17], chapters[18], chapters[19], chapters[20], chapters[21], chapters[22], chapters[23], chapters[24], chapters[25], chapters[26], chapters[27]]}
-              isMulti
-              name="questionchapters"
-              options={chapters}
-              onChange={(e) => this.setState({ selectedChapters: e })}
-              className="basic-multi-select"
-              classNamePrefix="select"
-            />
-          </div>
-        </div>
-      )
-    }
-    return ""
+    return (
+      <div>
+        <label htmlFor="questionChaptersLabel">Choose Chapters:</label>
+        <Select
+          defaultValue={[chapters[0], chapters[1], chapters[2], chapters[3], chapters[4], chapters[5], chapters[6], chapters[7], chapters[8], chapters[9], chapters[10], chapters[11], chapters[12], chapters[13], chapters[14], chapters[15], chapters[16], chapters[17], chapters[18], chapters[19], chapters[20], chapters[21], chapters[22], chapters[23], chapters[24], chapters[25], chapters[26], chapters[27]]}
+          isMulti
+          name="questionchapters"
+          options={chapters}
+          onChange={(e) => this.setState({ selectedChapters: e })}
+          className="basic-multi-select"
+          classNamePrefix="select"
+        />
+      </div>
+    )
   }
 
   showQuizMasterSection = () => {
-    var { username, full_question_text, answer_question_text, question_number, question_type, question_reference, is_bonus } = this.state
+    var { username, full_question_text, answer_question_text, question_reference } = this.state
     if (username === 'QM') {
-      let questionNumTemp;
-      if(is_bonus){
-        questionNumTemp = <h6 className="quizMasterBody"><b>Question:</b> Bonus</h6>
-      }else{
-        questionNumTemp = <h6 className="quizMasterBody"><b>Question:</b> #{question_number} </h6>
-      }
       return (
         <div className="quiz_master_content">
-          <div className="flex">
-            <h4 className="quizMasterBody" style={{flex:'1 100%','textAlign': 'left'}}><b>Full Question:</b> {full_question_text}</h4>
-            <h4 className="quizMasterBody" style={{flex:'1 100%','textAlign': 'left'}}><b>Answer:</b> {answer_question_text}</h4>
-            <div className="quizMasterBody flex" style={{flex:'1 100%'}}>
-              <h6 className="quizMasterBody"><b>Quiz:</b> {this.state.quizNumber}</h6>
-              {questionNumTemp}
-              <h6 className="quizMasterBody"><b>Question Reference:</b> {question_reference}</h6>
-            </div>
-            <h6 className="quizMasterBody"><b>Question type:</b> {question_type}</h6>
-            <h6 className="quizMasterBody wrap_around"><b>Quizzers in room:</b> {this.state.quizzers_in_room.join(', ')}</h6>
-          </div>
+          <h4 className="quizMasterBody"><b>Full Question:</b> {full_question_text}</h4>
+          <h4 className="quizMasterBody"><b>Answer:</b> {answer_question_text}</h4>
+          <h4 className="quizMasterBody"><b>Full Verse:</b> {question_reference}</h4>
+          <h6 className="quizMasterBody wrap_around"><b>Quizzers in room:</b> {this.state.quizzers_in_room.join(', ')}</h6>
           <div className="flex">
             <Button id="showMoreButton" onClick={() => this.showMoreQuizOptions()}>
               <ExpandLessIcon id="showLess"/>
@@ -368,15 +268,6 @@ current content of the editor to the server. */
             <h6>More Quiz Options</h6>
           </div>
           <div id="moreQuizOptions" className="flex">
-            <label htmlFor="roundSelector">Choose a quiz number: </label>
-            <select onChange={(e) => this.setQuizNumber(e.target.value)} name="quizSelector" id="quizSelector">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="practice">practice</option>
-            </select>
             <this.showMoreQuizControls></this.showMoreQuizControls>
           </div>
         </div>
@@ -388,26 +279,15 @@ current content of the editor to the server. */
   footerButtons = () => {
     var { username } = this.state
     if (username === 'QM') {
-      if(this.state.quizNumber === 'practice'){
-        return(
-          <div id="practiceQuiz" className="footerButton">
-            <Button onClick={() => this.randomQuestion()}><h3>Next Random Question</h3></Button>{' '}
-          </div>
-        )
-      }else{
-        let startQuizORnextQuestion = <div className="twoFooterButtons" ><Button style={{'left': '0'}} onClick={() => this.nextQuestion(false)}>Next Question</Button></div>
-        let bonusQuestion = <div className="twoFooterButtons" ><Button style={{'right': '0'}} onClick={() => this.nextQuestion(true, this.bonusQuestionNumber)}>Bonus Question</Button></div>
-        if(!this.state.quiz_started){
-          startQuizORnextQuestion = <div className="footerButton"><Button onClick={() => setInterval(() => this.startQuiz(), 1000)}><h2>Start Quiz</h2></Button></div>
-          bonusQuestion = ""
-        }
-        return (
-          <div id="realQuiz">
-            {startQuizORnextQuestion}{' '}
-            {bonusQuestion}{' '}
-          </div>
-        )
+      let startQuizORnextQuestion = <div id="practiceQuiz" className="footerButton"><Button onClick={() => this.nextQuestion()}><h3>Next Question</h3></Button>{' '}</div>
+      if(!this.state.quiz_started){
+        startQuizORnextQuestion = <div className="footerButton"><Button onClick={() => setInterval(() => this.startQuiz(), 1000)}><h2>Start Quiz</h2></Button></div>
       }
+      return (
+        <div id="realQuiz">
+          {startQuizORnextQuestion}{' '}
+        </div>
+      )
     }else{
       return (
         <div className="footerButton">
@@ -421,8 +301,6 @@ current content of the editor to the server. */
     const {
       q_text_to_display,
       question_number,
-      question_type,
-      is_bonus,
       room,
       username,
       quiz_started,
@@ -430,10 +308,8 @@ current content of the editor to the server. */
     } = this.state;
     let quizzerQuestionInformation;
     let quizzerQuestion=<h2>{q_text_to_display}</h2>
-    if(is_bonus){
-      quizzerQuestionInformation = <h4 className="question_information">Bonus Question: {question_type}</h4>
-    }else if(question_number>0){
-      quizzerQuestionInformation = <h4 className="question_information">#{question_number}: {question_type}</h4>
+    if(question_number>0){
+      quizzerQuestionInformation = <h4 className="question_information">Question #{question_number}</h4>
     }else{
       quizzerQuestion=<h2><span role="img" aria-label="eyes">👀</span> Watch for the question to appear here. <span role="img" aria-label="eyes">👀</span></h2>
       quizzerQuestionInformation = <h4 className="question_information">Quiz Master has not started the quiz.</h4>
--- src/webserviceCalls.js
+++ src/webserviceCalls.js
@@ -1,10 +1,12 @@
-const fetchQuestion = (questionID) => {    
-  return fetch('https://bible-questions-api.herokuapp.com/?QID='+questionID)            
-  // return fetch('http://localhost:5000/?QID='+questionID)
+export const fetchQuestion = (books, chapters) => {  
+  // return ({
+  //   book: "Mattew", 
+  //   chapter: "1", 
+  //   verse: "2", 
+  //   question: "Who was the father (of Isaac)?", 
+  //   answer: "Abraham OR Isaac OR Jacob", 
+  //   reference: "Abraham was the father of Isaac, and Isaac the father of Jacob, and Jacob the father of Judah and his brothers,", 
+  // })
+  return fetch('https://tfc-quizzing-api.herokuapp.com/filtered?books='+books+'&chapters='+chapters)
+  // return fetch('http://localhost:5000/filtered?books='+books+'&chapters='+chapters)
 }
-
-const fetchRandomQuestion = (qtype, books, chapters) => {    
-  return fetch('https://bible-questions-api.herokuapp.com/filtered?qtype='+qtype+'&books='+books+'&chapters='+chapters)
-  // return fetch('http://localhost:5000/filtered?qtype='+qtype+'&books='+books+'&chapters='+chapters)
-}
-export { fetchQuestion, fetchRandomQuestion } 
