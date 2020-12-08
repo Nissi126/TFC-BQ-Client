0ed3b5f34099d56c3760a37f12e1e80d60cd80b0
LAPTOP-CPQLSHDE\littlemag126 <callerofhope7@gmail.com>
2020-12-08 16:52:28 -0600

    Update Filtered request

--- src/webserviceCalls.js
+++ src/webserviceCalls.js
@@ -1,10 +1,10 @@
 const fetchQuestion = (books, chapters,league) => { 
-  var query='http://localhost:5000/filtered?league='+league+'&books='+books
+  var query='filtered?league='+league+'&books='+books
   for (const [key, value] of Object.entries(chapters)) {
     query =query +'&'+key+'='+value
   }
-  return fetch(query)
-  // return fetch(query)
+  return fetch('https://tfc-quizzing-api.herokuapp.com/'+query)
+  // return fetch('http://localhost:5000/'+query)
 }
 
 const fetchMaterial = (league) => {
